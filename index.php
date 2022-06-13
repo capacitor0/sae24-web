137afa2245a39d53b03eb73c737fda237b411099
capacitor0 <13393289+capacitor0@users.noreply.github.com>
2022-06-13 05:01:18 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <script src="https://kit.fontawesome.com/e6584b82db.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Chivo:400,700">
+    <meta name="author" content="Clément OZIOL">
+    <title>Projet Intégratif</title>
+</head>
+
+<body>
+    <header>
+        <h1>SAE24</h1>
+        <p><em>Projet intégratif</em></p>
+        <div>
+            <a href="index.php?page=home">Accueil</a>
+            <a href="index.php?page=topologie">Topologie</a>
+            <a href="index.php?page=devices">Matériels</a>
+            <a href="index.php?page=experience">Expérience</a>
+            <a href="index.php?page=attacks">Attaques</a>
+            <a href="index.php?page=skills">Compétences</a>
+            <a href="index.php?page=contact">Contact</a>
+        </div>
+    </header>
+    <main>
+        <section id="home">
+            <h1>Accueil</h1>
+            <div id="indent">
+                <p>Je m’appelle Clément OZIOL, je poursuis actuellement un Bachelor Universitaire de
+                    Technologie en Réseaux et Télécommunications à l'Institut universitaire de technologie de Clermont-Ferrand (63). On peut me distinguer par mon sérieux, ma curiosité et
+                    mon
+                    attention au détail ce qui m’est très utile dans ce secteur. Passionné d’informatique depuis le plus
+                    jeune âge j’ai rapidement développé un intérêt pour la cyber sécurité. Je souhaite donc m’orienter
+                    vers
+                    une école d’ingénieur pour me spécialiser d’autant plus dans ce domaine.</p>
+            </div>
+        </section>
+        <section id="cursus">
+            <h1>Cursus</h1>
+            <div id="indent">
+                <table>
+                    <tr>
+                        <td>Septembre 2021</td>
+                        <td>
+                            <p><b>Inscription en BUT</b>, <em>Institut Universistaire de Technologie</em>,
+                                Clermont-Ferrand (63)</p>
+                            <p><em>Réseaux et Télécommunications (R&T)</em></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Septembre 2020</td>
+                        <td>
+                            <p><b>Inscription en CPGE</b>, <em>Lycée Blaise-Pascal</em>, Clermont-Ferrand (63)</p>
+                            <p><em>Physique, Chimie et Sciences de l'Ingénieur (PCSI)</em></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Juillet 2020</td>
+                        <td>
+                            <p><b>Obtention du baccalauréat</b>, <em>Institution Saint-Alyre</em>, Clermont-Ferrand (63)
+                            </p>
+                            <p>Série Scientifique section européenne anglais</p>
+                            <p><em>Mention Bien</em></p>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </section>
+        <section id="skills">
+            <h1>Compétences</h1>
+            <em>Sur ⭐️⭐️⭐️⭐️⭐️</em>
+            <div id="indent">
+                <h3>Techniques</h3>
+                <div id="indent">
+                    <ul>
+                        <li id="part-info">Informatique ⭐️⭐️⭐️⭐️ :</li>
+                        <ul>
+                            <li>Administration système : Windows et Linux</li>
+                            <li>Déploiement de stations de travail</li>
+                            <li>Configuration de services (DHCP, FTP, NFS, NGINX)</li>
+                            <li>Gestion d'une infrastructure de virtualisation (Hyper-V, VMware vSphere)</li>
+                        </ul>
+                        <li id="part-res">Réseaux ⭐️⭐️⭐️ :</li>
+                        <ul>
+                            <li>Conception et administration d'un réseau</li>
+                            <li>Configuration d'équipements Cisco IOS</li>
+                        </ul>
+                        <li id="part-telco">Téléphonie et télécommunications ⭐️⭐️ :</li>
+                        <ul>
+                            <li>Déploiement d'une solution de communication sur IP</li>
+                        </ul>
+                        <li id="part-prog">Programmation ⭐️⭐️⭐️⭐️ :</li>
+                        <ul>
+                            <li>Développement et pilotage de projet</li>
+                            <li>Python, JavaScript, Bash</li>
+                            <li>C, WLangage</li>
+                            <li>Familier avec C#, Java et Go</li>
+                            <li>HTML, CSS</li>
+                        </ul>
+                        <li id="part-cyber">Cybersécurité ⭐️⭐️⭐️⭐️ :</li>
+                        <ul>
+                            <li>Mise en œuvre d'un système d’information sécurisé</li>
+                            <li>Sécurisation d'un réseau</li>
+                            <li>Mise en œuvre d'un système de surveillance d’incidents de sécurité</li>
+                            <li>Audit de la sécurité des systèmes</li>
+                        </ul>
+                    </ul>
+                </div>
+                <h3>Humaines</h3>
+                <div id="indent">
+                    <ul>
+                        <li>Autonomie ⭐️⭐️⭐️⭐️⭐️</li>
+                        <li>Sérieux ⭐️⭐️⭐️⭐️</li>
+                        <li>Réactivité ⭐️⭐️⭐️</li>
+                        <li>Investissement ⭐️⭐️⭐️⭐️⭐️</li>
+                        <li>Analyse ⭐️⭐️⭐️⭐️</li>
+                        <li>Assister d'autres personnes ⭐️⭐️⭐️</li>
+                    </ul>
+                </div>
+                <h3>Certifications</h3>
+                <div id="indent">
+                    <ul>
+                        <li>Cisco CCNA Routing and Switching</li>
+                    </ul>
+                </div>
+                <h3>Linguistiques</h3>
+                <div id="indent">
+                    <ul>
+                        <li>Anglais ⭐️⭐️⭐️⭐️</li>
+                        <li>Espagnol ⭐️⭐️⭐️</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section id="experience">
+            <h1>Expérience</h1>
+            <div id="indent">
+                <table>
+                    <tr>
+                        <td>Février 2020<br>Aujourd'hui</td>
+                        <td>
+                            <h4>Concepteur et superviseur du système d'information, VelOptimised, Clermont-Ferrand (63)
+                            </h4>
+                            <ul>
+                                <li>Déploiement, maintient et supervision d'une infrastructure d'hébergement web</li>
+                                <li>Optimisation et sécurisation</li>
+                                <li>Gestion d'un domaine DNS</li>
+                                <li>Gestion d'un serveur de courriel</li>
+                                <li>Configuration et administration d'un logiciel de CRM (<em>Customer Relationship
+                                        Management</em>)</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Mars 2018<br>Aujourd'hui</td>
+                        <td>
+                            <h4>Audit de sécurité, APLIM, Chambéry (73)</h4>
+                            <ul>
+                                <li>Audit de la sécurité réseau</li>
+                                <li>Audit de la sécurité web</li>
+                                <li>Audit de la sécurité logicielle</li>
+                                <li>Ingénierie inverse</li>
+                                <li>Tests d'intrusion</li>
+                                <li>Rédaction de rapports techniques</li>
+                            </ul>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </section>
+        <section id="hobbies">
+            <h1>Centres d'intêret</h1>
+            <div id="indent">
+                <p><b>Sport : </b>VTT, Course à pied</p>
+                <p><b>Voyage : </b>Chine (2017)</p>
+                <p><b>Autre : </b>Gestion de mes propres serveurs.<br>Investissement, finance</p>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <section id="cv">
+            <h1><em>Curriculum vitæ</em></h1>
+            <div id="indent">
+                <img src="res/uk.png"><img src="res/us.png">
+                <p>English version: </i><a href="cv/CV_OZIOL_EN.pdf" target="_blank">CV_OZIOL_EN.pdf</a></p>
+                <img src="res/fr.png">
+                <p>Version française: </i><a href="cv/CV_OZIOL_FR.pdf" target="_blank">CV_OZIOL_FR.pdf</a></p>
+            </div>
+        </section>
+        <section id="contact">
+            <h1>Contact</h1>
+            <div id="indent">
+                <p>E-mail: <em>clement.oziol (@) etu.uca.fr</em> | <em>clement (@) oziol.fr</em></p>
+                <p>GitHub: <i class="fab fa-github-square"></i><a href="https://github.com/capacitor0" target="_blank">
+                        capacitor0</a></p>
+            </div>
+        </section>
+    </footer>
+</body>
+
+</html>
